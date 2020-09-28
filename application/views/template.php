328b20beba52e6b24fdf1136dd9afa7cdda0b86f
nfaizin <nf.nurfaizin.nf@gmail.com>
2020-09-28 22:44:17 +0700

    testupdate

--- application/views/template.php
+++ application/views/template.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>myPOS - by ZinTech</title>
+  <title>myPOS1 - by ZinTech</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/font-awesome/css/font-awesome.min.css">
